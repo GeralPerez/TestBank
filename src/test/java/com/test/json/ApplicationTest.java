e40256e4628e7dd2040207a55e3db9c2b32df669
Geraldin Perez <gplopez@indracompany.com>
2020-08-24 15:50:49 -0500

    Aplicacion rama master

---
+++ src/test/java/com/test/json/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.test.json;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testHolaMundo() {
+        System.out.println("hola mundo");
+    }
+}
